d400342c21edaff7b8f9854cfdd5731882969260
ppmasa8 <kiyokiyopip56@gmail.com>
2023-06-21 22:27:39 +0900

    Added fix comment

--- lib/src/views/screens/group_view.dart
+++ lib/src/views/screens/group_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//TODO: FIX THIS
 class GroupView extends StatelessWidget {
   const GroupView({Key? key}) : super(key: key);
 
