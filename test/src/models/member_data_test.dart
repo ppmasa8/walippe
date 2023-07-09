1cfd27b54d4bb1934e8897d1bc81d0972ae166d9
ppmasa8 <kiyokiyopip56@gmail.com>
2023-07-09 15:36:34 +0900

    Added test.yml and test

---
+++ test/src/models/member_data_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:walippe/src/models/member_data.dart';
+
+void main() {
+  group('MemberData', () {
+    late MemberData memberData;
+
+    setUp(() {
+      memberData = MemberData(
+        id: 1,
+        groupId: 1,
+        name: 'John Doe',
+        description: 'Sample member',
+        balance: 100,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      );
+    });
+
+    test('should create a valid MemberData instance', () {
+      expect(memberData, isA<MemberData>());
+      expect(memberData.id, 1);
+      expect(memberData.groupId, 1);
+      expect(memberData.name, 'John Doe');
+      expect(memberData.description, 'Sample member');
+      expect(memberData.balance, 100);
+      expect(memberData.createdAt, isA<DateTime>());
+      expect(memberData.updatedAt, isA<DateTime>());
+    });
+
+    test('should have correct values when updating properties', () {
+      final updatedMemberData = memberData.copyWith(
+        name: 'Jane Smith',
+        description: 'Updated member',
+        balance: 200,
+      );
+
+      expect(updatedMemberData, isA<MemberData>());
+      expect(updatedMemberData.id, 1);
+      expect(updatedMemberData.groupId, 1);
+      expect(updatedMemberData.name, 'Jane Smith');
+      expect(updatedMemberData.description, 'Updated member');
+      expect(updatedMemberData.balance, 200);
+      expect(updatedMemberData.createdAt, memberData.createdAt);
+      expect(updatedMemberData.updatedAt, memberData.updatedAt);
+    });
+  });
+}
