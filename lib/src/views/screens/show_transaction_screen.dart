13c02e2003c5b2be4e882a55454925e2f6404061
ppmasa8 <kiyokiyopip56@gmail.com>
2023-07-29 18:45:59 +0900

    in progress

--- lib/src/views/screens/show_transaction_screen.dart
+++ lib/src/views/screens/show_transaction_screen.dart
@@ -2,32 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../models/transaction_data.dart';
+import '../../providers/provider.dart';
+
 class ShowTransactionScreen extends ConsumerWidget {
-  const ShowTransactionScreen({super.key});
+  const ShowTransactionScreen({super.key, required this.transactionData});
+
+  final TransactionData transactionData;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final transactionDetailListAsync = ref
+        .watch(transactionDetailListInTransactionProvider(transactionData.id));
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'transaction(仮)',
-          style: GoogleFonts.sawarabiGothic(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            transactionData.subject,
+            style: GoogleFonts.sawarabiGothic(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurpleAccent,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        body: Stack(
           children: [
-            const Text('transaction(仮)'),
+            transactionDetailListAsync.when(
+              data: (tDList) {
+                return ListView.separated(
+                  itemCount: tDList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(tDList[index].payeeId.toString()),
+                      trailing: Text(tDList[index].amount.toString()),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                );
+              },
+              loading: () => const CircularProgressIndicator(),
+              error: (error, stackTrace) => Text('Error: $error'),
+            )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
